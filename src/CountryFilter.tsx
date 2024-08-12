ca6d0ffffbd1b86c06a3f0d73f2e254dfe72bb9b
sg <sg@iboxindia.com>
2024-08-12 15:26:01 +0530

    pulled latest expo changes

---
+++ src/CountryFilter.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { TextInput, StyleSheet, TextInputProps, Platform } from "react-native";
+import { useTheme } from "./CountryTheme";
+
+const styles = StyleSheet.create({
+  input: {
+    height: 48,
+    width: "70%",
+    ...Platform.select({
+      web: {
+        outlineWidth: 0,
+        outlineColor: "transparent",
+        outlineOffset: 0,
+      },
+    }),
+  },
+});
+
+export type CountryFilterProps = TextInputProps;
+
+export const CountryFilter = ({
+  placeholder,
+  ...props
+}: CountryFilterProps) => {
+  const {
+    filterPlaceholderTextColor,
+    fontFamily,
+    fontSize,
+    onBackgroundTextColor,
+  } = useTheme();
+  return (
+    <TextInput
+      testID="text-input-country-filter"
+      autoCorrect={false}
+      placeholderTextColor={filterPlaceholderTextColor}
+      style={[
+        styles.input,
+        { fontFamily, fontSize, color: onBackgroundTextColor },
+      ]}
+      placeholder={placeholder || "Enter country name"}
+      {...props}
+    />
+  );
+};
